3850f22b58cba6d4ff804b19fb410d0b9672e8f7
saket2001 <saketchandorkar123@gmail.com>
2021-08-17 15:24:53 +0530

    fixed footer css

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import instalogo from "../../public/Insta_logo1.png";
 import facebooklogo from "../../public/facebook_logo.png";
 import twitterlogo from "../../public/twiiter_logo.png";
@@ -9,60 +10,55 @@ const Footer = () => {
   return (
     <div className={styles.main}>
       <div className={styles.footer}>
-        <div className={styles.Aboutus}>
+        <div className={styles.footer__item}>
           <h4>BechDaal</h4>
           <p>4041 Cunningham Court Farmington Hills MI Michigan 48335.</p>
-          <a href="/">page link</a>
+          <Link href="/">Home</Link>
         </div>
-        <div className={styles.featuredpro}>
-          <h4>Featured Products</h4>
+        <div className={styles.footer__item}>
+          <h4>Our Services</h4>
           <ul>
             <li>
-              <a href="/">Electronics</a>
+              <Link href="/category">Shop By Category</Link>
             </li>
             <li>
-              <a href="/">Bikes</a>
+              <Link href="/sellproduct">Sell product</Link>
             </li>
             <li>
-              <a href="/">Cars</a>
+              <Link href="/contactus">Contact us</Link>
             </li>
             <li>
-              <a href="/">Books</a>
+              <Link href="/usersauth">Log in</Link>
             </li>
             <li>
-              <a href="/">Furniture</a>
+              <Link href="/">Our Policy</Link>
             </li>
           </ul>
         </div>
-        <div className={styles.Contactus}>
+        <div className={styles.footer__item}>
           <h4>Contact us</h4>
           <ul>
             <li>BechDaal@gmail.com</li>
             <li>Service no: 022-1234567/89</li>
           </ul>
         </div>
-        <div className={styles.Social}>
-          <Image
-            className={styles.img}
-            src={instalogo}
-            alt="Insta logo"
-            width="65x"
-            height="65px"
-          />
-          <Image
-            className={styles.img}
-            src={facebooklogo}
-            alt="facebook logo"
-            width="65px"
-            height="65px"
-          ></Image>
-          <Image
-            className={styles.img}
-            src={twitterlogo}
-            alt="twitter logo"
-            width="65px"
-            height="65px"
-          ></Image>
+        <div className={styles.footer__item}>
+          <h4>Follow us</h4>
+          <div className={styles.footer__icons}>
+            <Image src={instalogo} alt="Insta logo" width="45x" height="45px" />
+            <Image
+              src={facebooklogo}
+              alt="facebook logo"
+              width="45px"
+              height="45px"
+            ></Image>
+            <Image
+              src={twitterlogo}
+              alt="twitter logo"
+              width="45px"
+              height="45px"
+            ></Image>
+          </div>
         </div>
       </div>
       <div>
